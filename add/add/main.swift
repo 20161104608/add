c0ebd4024308e4ae6ff2ca5a5cfc5352db2d605f
20161104608 <a20161104608@admindeiMac-36.local>
2018-09-12 11:29:37 +0800

    add
    add

---
+++ add/add/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  add
+//
+//  Created by 20161104608 on 18/9/12.
+//  Copyright © 2018年 20161104608. All rights reserved.
+//
+
+import Foundation
+
+
+var sum = 0
+var a = 1
+for i in 1...100{
+    
+    sum = sum+i
+    
+}
+
+print(sum)
+
+
